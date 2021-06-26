f1b43dacfa97eafd073a1576a6abc294569c101e
Saharsh Shukla <saharsh.shukla2018@vitstudent.ac.in>
2021-06-26 18:30:44 +0530

    add FactProblem

--- DSA-CPP/Codechef/Begineer/smallfactorials.cpp
+++ DSA-CPP/Codechef/Begineer/smallfactorials.cpp
@@ -1,21 +1,57 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+// Fails for 2^64
 
-long long factorial(int number) {
-    long long factorial = 1;
-    for (int i = 1; i < number+1; i++) {
-        factorial = factorial * i;
+vector<int> f(vector<int> arr, int b) {
+    int size = arr.size();
+    int temp = 0;
+    for (int i = 0; i < size; i++)
+    {
+        int x = (arr[i] * b + temp);
+        temp = x / 10;
+        arr[i] = x%10;
+        // cout << arr[i] << " ";
     }
-    return factorial;
+    while (temp != 0)
+    {
+        arr.push_back(temp % 10);
+        temp = temp / 10;
+    }
+    return arr;
 }
 
-int main() {
-    int TESTCASES;
-    cin >> TESTCASES;
-    for (int i =0; i < TESTCASES; i++) {
-        int temp;
-        cin >> temp;
-        cout << factorial(temp) << endl;
+
+vector<int> computeArray(int number)
+{
+    vector<int> numberarray;
+    while (number != 0)
+    {
+        numberarray.push_back(number % 10);
+        number = number / 10;
+    }
+
+    return numberarray;
+}
+
+int main()
+{
+    int testcases;
+    cin >> testcases;
+    for (int i = 0; i < testcases; i++) {
+        int number;
+        cin >> number;
+        vector <int> array = computeArray(number);
+
+        int num = number -1;
+        while (num>=1) {
+            array = f(array, num);
+            num--;
+        }
+
+        for (int i =array.size()-1; i >= 0; i--) {
+            cout << array[i];
+        }
+        cout << endl;
     }
 }
