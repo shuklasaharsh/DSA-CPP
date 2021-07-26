462e003285beeb1962804524b72717ae8dc93c4a
Saharsh Shukla <saharsh.shukla2018@vitstudent.ac.in>
2021-07-26 11:56:08 +0530

    add Strings

---
+++ DSA-CPP/CodingBlocks/String-Challenges/6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+int main() {
+
+    // Inputs
+    int replacements_allowed;
+    cin >>replacements_allowed;
+    cin.get();
+    string a;
+    getline(cin, a);
+    cout << a;
+
+}
