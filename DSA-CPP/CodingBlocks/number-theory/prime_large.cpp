93b6152ac812b98b1e0a92257bdd3d2e6cb21a76
saharshshukla <saharsh.shukla2018@vitstudent.ac.in>
2021-08-17 16:58:26 +0530

    SWITCH

---
+++ DSA-CPP/CodingBlocks/number-theory/prime_large.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bitset>
+#include <vector>
+
+#define ll long long
+
+using namespace std;
+
+const int n = 10000000;
+bitset<10000005> b;
+vector <int> primes;
+bool isPrime(ll No) {
+    if (No <= n) {
+        return b
+    }
+}
+
+int prime_sieve(int *arr) {
+
+}
+
+int main() {
+
+    return 0;
+}
